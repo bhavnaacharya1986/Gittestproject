8ead85c71b3ddd3ea4e35d2729d71e0f65f2b510
bhavnaacharya1986 <153376812+bhavnaacharya1986@users.noreply.github.com>
2023-12-20 14:58:29 +0000

    coded finalised and added

---
+++ src/test/java/class_1.java
@@ -0,0 +1,12 @@
+public class class_1 {
+
+    public static void main(String[] args) {
+        int i = 23;
+        int j = 34;
+        int k = 45;
+        int l = (j / i) + k;
+
+        System.out.println(l);
+
+    }
+}
---
+++ src/test/java/class_2.java
@@ -0,0 +1,11 @@
+public class class_2 {
+
+    public static void main(String[] args) {
+        int i = 23;
+        int j = 34;
+        int k = 45;
+        int l = i + j + k;
+
+        System.out.println(l);
+    }
+}
